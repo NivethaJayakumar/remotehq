534d3ecce5db4bbc8f41d43f75d671dd012f7f8a
Nivetha Jayakumar <83976606+NivethaJayakumar@users.noreply.github.com>
2021-09-16 13:09:50 -0700

    Add files via upload

--- src/Avatars.js
+++ src/Avatars.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Avatars = () => {
     return (
         <div className="flex -space-x-1 overflow-hidden">
-        <img className="inline-block h-6 w-6 rounded-full ring-2 ring-white" src="https://avatar-prod-us-east-2.webexcontent.com/Avtr~V1~1eb65fdf-9643-417f-9974-ad72cae0e10f/V1~47c32d0b-0444-460d-8bce-2265b501aac5~311b7801865d484fb072dd0d5e8374bd~1600" alt="Arash"/>
-        <img className="inline-block h-6 w-6 rounded-full ring-2 ring-white" src="https://avatar-prod-us-east-2.webexcontent.com/Avtr~V1~1eb65fdf-9643-417f-9974-ad72cae0e10f/V1~8c5cfa04-1b0c-4ab2-be06-1b8c46098005~defc8f7cc94d4ec1abd3b9d71c27bccc~1600" alt="Ashesh"/>
-        <img className="inline-block h-6 w-6 rounded-full ring-2 ring-white" src="https://avatar-prod-us-east-2.webexcontent.com/Avtr~V1~1eb65fdf-9643-417f-9974-ad72cae0e10f/V1~c2b605da-484e-42ff-8cc5-cb2ea8d21480~ee9b71a2768a4f3399847f740c5916a2~1600" alt="Nivetha"/>
-        <img className="inline-block h-6 w-6 rounded-full ring-2 ring-white" src=" https://avatar-prod-us-east-2.webexcontent.com/Avtr~V1~1eb65fdf-9643-417f-9974-ad72cae0e10f/V1~7b383e03-f175-4ae4-b60a-6d8b378d2ffd~cde996507221414a8474c687450fd24f~1600" alt="Morgan"/>
-        <img className="inline-block h-6 w-6 rounded-full ring-2 ring-white" src="https://avatar-prod-us-east-2.webexcontent.com/Avtr~V1~1eb65fdf-9643-417f-9974-ad72cae0e10f/V1~cf98e47f-974b-4bea-9c91-4e2840762222~f1d71e93b5af43aa827c4a74f4fe18e7~1600" alt="Rajitha"/>
-        <img className="inline-block h-6 w-6 rounded-full ring-2 ring-white" src="https://avatar-prod-us-east-2.webexcontent.com/Avtr~V1~1eb65fdf-9643-417f-9974-ad72cae0e10f/V1~77f84128-9625-44b3-a145-46e700327ebf~095fda2b54464f61944c8e8c44861301~1600" alt="Kenneth"/>
+        <img className="inline-block h-8 w-8 rounded-full ring-2 ring-white" src="https://avatar-prod-us-east-2.webexcontent.com/Avtr~V1~1eb65fdf-9643-417f-9974-ad72cae0e10f/V1~47c32d0b-0444-460d-8bce-2265b501aac5~311b7801865d484fb072dd0d5e8374bd~1600" alt="Arash"/>
+        <img className="inline-block h-8 w-8 rounded-full ring-2 ring-white" src="https://avatar-prod-us-east-2.webexcontent.com/Avtr~V1~1eb65fdf-9643-417f-9974-ad72cae0e10f/V1~8c5cfa04-1b0c-4ab2-be06-1b8c46098005~defc8f7cc94d4ec1abd3b9d71c27bccc~1600" alt="Ashesh"/>
+        <img className="inline-block h-8 w-8 rounded-full ring-2 ring-white" src="https://avatar-prod-us-east-2.webexcontent.com/Avtr~V1~1eb65fdf-9643-417f-9974-ad72cae0e10f/V1~c2b605da-484e-42ff-8cc5-cb2ea8d21480~ee9b71a2768a4f3399847f740c5916a2~1600" alt="Nivetha"/>
+        <img className="inline-block h-8 w-8 rounded-full ring-2 ring-white" src=" https://avatar-prod-us-east-2.webexcontent.com/Avtr~V1~1eb65fdf-9643-417f-9974-ad72cae0e10f/V1~7b383e03-f175-4ae4-b60a-6d8b378d2ffd~cde996507221414a8474c687450fd24f~1600" alt="Morgan"/>
+        <img className="inline-block h-8 w-8 rounded-full ring-2 ring-white" src="https://avatar-prod-us-east-2.webexcontent.com/Avtr~V1~1eb65fdf-9643-417f-9974-ad72cae0e10f/V1~cf98e47f-974b-4bea-9c91-4e2840762222~f1d71e93b5af43aa827c4a74f4fe18e7~1600" alt="Rajitha"/>
+        <img className="inline-block h-8 w-8 rounded-full ring-2 ring-white" src="https://avatar-prod-us-east-2.webexcontent.com/Avtr~V1~1eb65fdf-9643-417f-9974-ad72cae0e10f/V1~77f84128-9625-44b3-a145-46e700327ebf~095fda2b54464f61944c8e8c44861301~1600" alt="Kenneth"/>
         </div>
     );
 }
